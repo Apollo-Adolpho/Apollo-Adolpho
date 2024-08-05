4198c5c1be5a826b6b4aa45f84b44a2934d2afd7
Apollo-Adolpho <apollo.mattos@escola.pr.gov.br>
2024-08-05 08:34:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,63 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
+const mainContainer = document.querySelector(".main-container");
+const containerQuestions = document.querySelector(".container-questions");
+const containerAlternatives = document.querySelector(".container-alternatives");
+const containerResult = document.querySelector(".container-result");
+const textResult = document.querySelector(".text-result");
 
-const perguntas = [
+const questions = [
     {
-        enunciado: "Após a escola, caminho à sua casa, você se depara com algo brilhanto em meio a uma árvore que recentemente caiu após uma tempestade. O que você faz?",
-        alternativas: [
+        qTitle: "Após a escola, caminho à sua casa, você se depara com algo brilhanto em meio a uma árvore que recentemente caiu após uma tempestade. O que você faz?",
+        alternatives: [
             "Vai averiguar",
             "Ignora e vai para casa"
         ]
     },
     {
-        enunciado: "Pergunta 2",
-        alternativas: [
+        qTitle: "Pergunta 2",
+        alternatives: [
             "Alternativa 1",
             "Alternativa 2"
         ]
     },
     {
-        enunciado: "Pergunta 3",
-        alternativas: [
+        qTitle: "Pergunta 3",
+        alternatives: [
             "Alternativa 1",
             "Alternativa 2"
         ]
     },
     {
-        enunciado: "Pergunta 4",
-        alternativas: [
+        qTitle: "Pergunta 4",
+        alternatives: [
             "Alternativa 1",
             "Alternativa 2"
         ]
     },
     {
-        enunciado: "Pergunta 5",
-        alternativas: [
+        qTitle: "Pergunta 5",
+        alternatives: [
             "Alternativa 1",
             "Alternativa 2"
         ]
     }
 ];
 
-let atual = 0;
-let perguntaAtual;
+let current = 0;
+let currentQuestion;
 
-function mostraPergunta() {
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
+
+function showQuestion() {
+    currentQuestion = questions[current];
+    containerQuestions.textContent = currentQuestion.qTitle;
+    showAlternatives();
 }
+function showAlternatives() {
+    for (const alternative of currentQuestion.alternatives) {
+        const alternativesButton = document.createElement("button")
+        alternativesButton.textContent = alternative
+        alternativesContainer.appendChild(alternativesButton)
+    }
+  }
+  
 
-mostraPergunta()
+  showQuestion()
